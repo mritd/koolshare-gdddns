34fdba31a1818e64b9d7d52abbd750fa198f3108
mritd <mritd1234@gmail.com>
2017-04-29 22:45:53 +0800

    增加更新命令

--- gdddns/scripts/gdddns_update.sh
+++ gdddns/scripts/gdddns_update.sh
@@ -8,40 +8,22 @@ if [ "$gdddns_enable" != "1" ]; then
 fi
 
 now=`date`
+ip=`$gdddns_curl 2>&1` || die "$ip"
+current_ip=`nslookup $gdddns_name.$gdddns_domain $gdddns_dns 2>&1`
+
+[ "$gdddns_curl" = "" ] && gdddns_curl="curl -s whatismyip.akamai.com"
+[ "$gdddns_dns" = "" ] && gdddns_dns="8.8.8.8"
+[ "$gdddns_ttl" = "" ] && gdddns_ttl="600"
 
 die () {
     echo $1
     dbus ram gdddns_last_act="$now: failed($1)"
 }
 
-[ "$gdddns_curl" = "" ] && gdddns_curl="curl -s whatismyip.akamai.com"
-[ "$gdddns_dns" = "" ] && gdddns_dns="ns05.domaincontrol.com"
-[ "$gdddns_ttl" = "" ] && gdddns_ttl="600"
-
-ip=`$gdddns_curl 2>&1` || die "$ip"
-
-current_ip=`nslookup $gdddns_name.$gdddns_domain $gdddns_dns 2>&1`
-
-if [ "$?" -eq "0" ]
-then
-    current_ip=`echo "$current_ip" | grep 'Address 1' | tail -n1 | awk '{print $NF}'`
-
-    if [ "$ip" = "$current_ip" ]
-    then
-        echo "skipping"
-        dbus set gdddns_last_act="$now: skipped($ip)"
-        exit 0
-    fi 
-fi
-
-
-timestamp=`date -u "+%Y-%m-%dT%H%%3A%M%%3A%SZ"`
-
 urlencode() {
     # urlencode <string>
     out=""
-    while read -n1 c
-    do
+    while read -n1 c; do
         case $c in
             [a-zA-Z0-9._-]) out="$out$c" ;;
             *) out="$out`printf '%%%02X' "'$c"`" ;;
@@ -59,6 +41,20 @@ update_record() {
         -H "Content-type: application/json" "https://api.godaddy.com/v1/domains/$gdddns_domain/records/A/$(enc "$gdddns_name")" \
         -d "{\"data\":\"$ip\",\"ttl\":$gdddns_ttl}")
 }
+
+
+if [ "$?" -eq "0" ]; then
+    current_ip=`echo "$current_ip" | grep 'Address 1' | tail -n1 | awk '{print $NF}'`
+
+    if [ "$ip" = "$current_ip" ]; then
+        echo "skipping"
+        dbus set gdddns_last_act="$now: skipped($ip)"
+        exit 0
+    else
+        update_record
+    fi 
+fi
+
 
 if [ "$ip" == "$current_ip" ];then
     dbus ram gdddns_last_act="$now: failed"
