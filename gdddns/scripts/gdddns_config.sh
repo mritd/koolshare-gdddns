3c12add5dea753c9440bc2c70b2e29c5f81b5c08
mritd <mritd1234@gmail.com>
2017-05-01 12:16:14 +0800

    修复config 脚本无法调用问题

--- gdddns/scripts/gdddns_config.sh
+++ gdddns/scripts/gdddns_config.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-echo "config" >> /tml/ddns.log
+echo "config" >> /tmp/ddns.log
 
 if [ "`dbus get gdddns_enable`" == "1" ]; then
-	echo "config open" >> /tmp/ddns.log
+    echo "config open" >> /tmp/ddns.log
     dbus delay gdddns_timer `dbus get gdddns_interval` /koolshare/scripts/gdddns_update.sh
 else
-	echo "config off" >> /tmp/ddns.log
+    echo "config off" >> /tmp/ddns.log
     dbus remove __delay__gdddns_timer
 fi
--- gdddns/scripts/gdddns_update.sh
+++ gdddns/scripts/gdddns_update.sh
@@ -37,7 +37,7 @@ enc() {
 }
 
 update_record() {
-    curl -kLsX PUT -H "Authorization: sso-key $gdddns_ak:$gdddns_sk" \
+    curl -kLsX PUT -H "Authorization: sso-key $gdddns_key:$gdddns_secret" \
         -H "Content-type: application/json" "https://api.godaddy.com/v1/domains/$gdddns_domain/records/A/$(enc "$gdddns_name")" \
         -d "{\"data\":\"$ip\",\"ttl\":$gdddns_ttl}"
 }
