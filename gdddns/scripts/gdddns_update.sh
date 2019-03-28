f3951c086de53ffffb8aa408d70d8cd43f0e80cd
tankren <9392749+tankren@users.noreply.github.com>
2019-03-28 13:38:46 +0800

    Update gdddns_update.sh
    update coding to adapt to the latest API rules

--- gdddns/scripts/gdddns_update.sh
+++ gdddns/scripts/gdddns_update.sh
@@ -7,19 +7,6 @@ if [ "$gdddns_enable" != "1" ]; then
     exit
 fi
 
-now=`date`
-ip=`$gdddns_curl 2>&1` || die "$ip"
-current_ip=`nslookup $gdddns_name.$gdddns_domain $gdddns_dns 2>&1`
-
-[ "$gdddns_curl" = "" ] && gdddns_curl="curl -s whatismyip.akamai.com"
-[ "$gdddns_dns" = "" ] && gdddns_dns="114.114.114.114"
-[ "$gdddns_ttl" = "" ] && gdddns_ttl="600"
-
-die () {
-    echo $1
-    dbus ram gdddns_last_act="$now: failed($1)"
-}
-
 urlencode() {
     # urlencode <string>
     out=""
@@ -37,23 +24,45 @@ enc() {
 }
 
 update_record() {
-    curl -kLsX PUT -H "Authorization: sso-key $gdddns_key:$gdddns_secret" \
-        -H "Content-type: application/json" "https://api.godaddy.com/v1/domains/$gdddns_domain/records/A/$(enc "$gdddns_name")" \
-        -d "[{\"data\":\"$ip\",\"ttl\":$gdddns_ttl}]"
+    curl -kLsX PUT "https://api.godaddy.com/v1/domains/$gdddns_domain/records/A/$(enc "$gdddns_name")" \
+                -H "accept: application/json" \
+                -H "Content-type: application/json" \
+                -H "Authorization: sso-key $gdddns_key:$gdddns_secret" \
+        -d "[{\"data\":\"$ip\",\"ttl\":$gdddns_ttl,\"port\":443,\"protocol\":\"https\",\"weight\":1}]"
 }
 
+record_response() {
+        curl -kLsX GET -H "Authorization: sso-key $gdddns_key:$gdddns_secret" \
+                -H "Content-type: application/json" "https://api.godaddy.com/v1/domains/$gdddns_domain/records/A/$(enc "$gdddns_name")"
+}
+
+now=`date '+%Y-%m-%d %H:%M:%S'`
+ip=`$gdddns_curl 2>&1` || die "$ip"
+
+[ "$gdddns_curl" = "" ] && gdddns_curl="curl -s whatismyip.akamai.com"
+[ "$gdddns_dns" = "" ] && gdddns_dns="114.114.114.114"
+[ "$gdddns_ttl" = "" ] && gdddns_ttl="600"
+
+die () {
+    echo $1
+    dbus ram gdddns_last_act="$now: failed($1)"
+}
 
 if [ "$?" -eq "0" ]; then
-    current_ip=`echo "$current_ip" | grep 'Address 1' | tail -n1 | awk '{print $NF}'`
-
+    current_ip=`record_response | grep -oE '([0-9]{1,3}\.?){4}'|head -n 1`
     if [ "$ip" = "$current_ip" ]; then
         echo "skipping"
-        dbus set gdddns_last_act="$now: skipped($ip)"
+#                new_ip=`record_response | grep -oE '([0-9]{1,3}\.?){4}'|head -n 1` 
+        dbus set gdddns_last_act="$now: 跳过更新,路由器IP:($ip),A记录IP:($current_ip)"
         exit 0
     else
         echo "changing"
         update_record
-        dbus set gdddns_last_act="$now: changed($ip)"
+                new_ip=`record_response | grep -oE '([0-9]{1,3}\.?){4}'|head -n 1`
+           if [ "$new_ip" = "$ip" ]; then
+            dbus set gdddns_last_act="$now: 更新成功,路由器IP:($ip),A记录IP:($new_ip)"
+            else
+            dbus set gdddns_last_act="$now: 更新失败!请检查设置"
+           fi
     fi 
 fi
-
